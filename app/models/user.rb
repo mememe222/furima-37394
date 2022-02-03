14b7b802e3acc9e168ff647dfe31d20638ad5bf8
mememe222 <mememe@hamabenaotonoiMac.local>
2022-02-03 09:48:03 +0900

    ユーザ管理③

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        validates :email, presence: true
         validates :nickname, presence: true
         validates :last_name, presence: true,
         format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
