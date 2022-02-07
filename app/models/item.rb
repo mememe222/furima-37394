9965ea8065126eefea866265a048dd704a16b679
mememe222 <mememe@hamabenaotonoiMac.local>
2022-02-07 12:19:31 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,6 @@ class Item < ApplicationRecord
   validates :shipment_source_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :shipping_days_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
 end
 
