ff013ca59e464dba25d934d5bb544db5e13f55bd
mememe222 <mememe@hamabenaotonoiMac.local>
2022-02-08 11:25:57 +0900

    修正③

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     title                 { 'test' }
     explanation           { 'explanation_test' }
-    category_id           { '2' }
-    condition_id          { '2' }
-    shipment_source_id    { '2' }
-    shipping_days_id      { '2' }
-    delivery_charge_id    { '2' }
+    category_id           { 2 }
+    condition_id          { 2 }
+    shipment_source_id    { 2 }
+    shipping_days_id      { 2 }
+    delivery_charge_id    { 2 }
     price                 { '1111' }
     association :user
 
