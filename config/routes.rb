0f3766c1890910f99d9906c057e409a8eb2e9733
mememe222 <mememe@hamabenaotonoiMac.local>
2022-02-08 23:08:37 +0900

    詳細表示②

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items, only: [:new, :index, :create, :show]
   post 'items/new', to: 'items#new'
+  resources :users, only: [:show]
 end
